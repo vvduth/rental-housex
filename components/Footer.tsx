896663a72b8f487467374d6ae1a4a91430705e0a
vvduth <ducthai060501@gmail.com>
2025-03-25 09:45:39 +0200

    Enhance Footer and InfoBoxes components with layout and styling improvements

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,43 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer
+     className='bg-gray-200 py-4 mt-auto'
+    >
+      <div className='container mx-auto flex flex-col
+      md:flex-row items-center justify-between px-4'>
+        <div className='mb-4 md:mb-0'>
+          <Image
+            src={'/images/logo.png'}
+            alt='Logo'
+            className='h-8 w-auto'
+            width={30}
+            height={30}
+          />
+        </div>
+        <div className='flex flex-wrap justify-center
+        md:justify-start mb-4 md:mb-0'>
+          <ul className='flex space-x-4'>
+            <li><Link href={'/properties'}>
+            Propterties</Link></li>
+            <li>
+              <Link
+              href={'/'}>
+              Terms of Service</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className='text-sm text-gray-500 mt-2
+          md:mt-0'>
+            &copy; {new Date().getFullYear()} Dukeroo
+          </p>
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- components/InfoBoxes.tsx
+++ components/InfoBoxes.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const InfoBoxes = () => {
   return (
-    <div>InfoBoxes</div>
-  )
-}
+    <section>
+      <div className="container-xl lg:container m-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+          <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold">For renters</h2>
+            <p className="mt-2 mb-4">
+              Find your fav properties. Bookmark them and contact the owners.
+            </p>
+            <Link
+              href={"/properties"}
+              className="inline-block bg-black text-white
+            rounded-lg px-4 py-2 hover:bg-gray-700"
+            >
+              Show me houses
+            </Link>
+          </div>
+          <div className="bg-blue-100 p-6 rounded-lg shadow-md">
+          <h2 className="text-2xl font-bold">For property owners</h2>
+          <p className="mt-2 mb-4">
+            List your properties and reach potential clients/tenant. Rent as
+            short term/long term
+          </p>
+          <Link
+            href={"/properties/add"}
+            className="inline-block bg-blue-500 text-white rounded-lg px-4 py-2
+          hover:bg-blue-600"
+          >
+            Add Properties
+          </Link>
+        </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default InfoBoxes
+export default InfoBoxes;
