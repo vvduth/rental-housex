535ebb5640dde245c006405d3cc1b7bdab64815b
vvduth <ducthai060501@gmail.com>
2025-03-28 09:34:37 +0200

    Add PropertySearchForm to PropertiesPage and create SearchResultPage for displaying search results

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,32 +1,37 @@
 import React from "react";
 import PropertyCard from "@/components/PropertyCard";
 import connectDB from "@/config/db";
-import {Property} from '@/models/Property'
+import { Property } from "@/models/Property";
 import { PropertyInterface } from "@/types";
-import { Document } from 'mongoose';
+import { Document } from "mongoose";
+import PropertySearchForm from "@/components/PropertySearchForm";
 const PropertiesPage = async () => {
+  await connectDB();
+  const properties = (await Property.find(
+    {}
+  ).lean()) as unknown as (PropertyInterface & Document)[];
 
-  await connectDB()
-  const properties = await Property.find({}).lean() as unknown as (PropertyInterface & Document)[]
-  
- 
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <p>No properties found</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard
-                key={property._id}
-                property={property}
-              />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-start">
+          <PropertySearchForm />
+        </div>
+      </section>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 
---
+++ app/properties/search-results/page.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import Link from "next/link";
+import connectDB from "@/config/db";
+import { Property } from "@/models/Property";
+import { convertToSerializeableObject } from "@/utils/covertToObject";
+import PropertyCard from "@/components/PropertyCard";
+import PropertySearchForm from "@/components/PropertySearchForm";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { PropertyInterface } from "@/types";
+const SearchResultPage = async ({
+  searchParams: { location, propertyType },
+}: {
+  searchParams: { location: string; propertyType: string };
+}) => {
+  await connectDB();
+  const locationReg = new RegExp(location, "i");
+  // Match location pattern against database fields
+  let query: {
+    $or: (
+      | { name: RegExp }
+      | { description: RegExp }
+      | { "location.street": RegExp }
+      | { "location.city": RegExp }
+      | { "location.state": RegExp }
+      | { "location.zipcode": RegExp }
+    )[];
+    type?: RegExp;
+  } = {
+    $or: [
+      { name: locationReg },
+      { description: locationReg },
+      { "location.street": locationReg },
+      { "location.city": locationReg },
+      { "location.state": locationReg },
+      { "location.zipcode": locationReg },
+    ],
+  };
+
+  if (propertyType && propertyType !== "All") {
+    const typePattern = new RegExp(propertyType, "i");
+    query.type = typePattern;
+  }
+  const propertiesQueryResults = await Property.find(query).lean();
+  const properties = convertToSerializeableObject(
+    propertiesQueryResults
+  ) as PropertyInterface[];
+  return (
+    <>
+      <section className="bg-blue-700 py-4">
+        <div
+          className="max-w-7xl mx-auto px-4 flex flex-col
+        items-start sm:px-6 lg:px-8"
+        >
+          <PropertySearchForm />
+        </div>
+      </section>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <Link
+            href={"/properties"}
+            className="flex items-center text-blue-500 hover:underline mb-3"
+          >
+            <FaArrowAltCircleLeft className="mr-2 mb-1" />
+            Back to properties
+          </Link>
+          <h1 className="text-2xl mb-4 font-semibold">Search Results</h1>
+          {properties.length === 0 ? (
+            <p>No search results found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default SearchResultPage;
